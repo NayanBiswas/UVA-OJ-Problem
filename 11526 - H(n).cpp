59ab22a373db47319f4133815a3d3f013a467cc8
Nayan Biswas <nayan.bi@yahoo.com>
2016-12-24 15:05:59 +0600

    C++

---
+++ 11526 - H(n).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+#include <cstdio>
+using namespace std;
+long long H(long long n){
+    long long res = 0, i, root;
+    if(n<=0) return 0;
+    root = sqrt(n);
+    for(i=1; i<=root; i++)
+        res += n/i;
+    res = res*2 - root*root;
+    return res;
+}
+int main()
+{
+    long long ans, test, t, n;
+    while(scanf("%lld",&test)!=EOF){
+        for(t=1; t<=test; t++){
+            scanf("%lld",&n);
+            ans = H(n);
+            printf("%lld\n",ans);
+        }
+    }
+    return 0;
+}
